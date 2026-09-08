85c5f69c75581a432caa282596dd2d04b47bedd5
Abenil6 <abenilee740@gmail.com>
2026-09-08 22:43:24 +0300

    feat: re-enable Custom Cursor with magnetic effects

--- portfolio-react/src/App.tsx
+++ portfolio-react/src/App.tsx
@@ -2,18 +2,23 @@ import { useLenis } from "./hooks/useLenis";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import ServicesManagement from "./pages/ServicesManagement";
+import CustomCursor from "./components/CustomCursor";
+import { CursorProvider } from "./hooks/useCursor";
 import "./App.css";
 
 function App() {
   useLenis();
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Portfolio />} />
-        <Route path="/admin/services" element={<ServicesManagement />} />
-      </Routes>
-    </Router>
+    <CursorProvider>
+      <CustomCursor />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+          <Route path="/admin/services" element={<ServicesManagement />} />
+        </Routes>
+      </Router>
+    </CursorProvider>
   );
 }
 

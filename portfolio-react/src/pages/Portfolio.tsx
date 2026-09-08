ade45b61cbcd9c6615e059a878cc9b75a02669eb
Abenil6 <abenilee740@gmail.com>
2026-09-08 22:38:52 +0300

    fix: temporarily disable all new features to debug loading issue

--- portfolio-react/src/pages/Portfolio.tsx
+++ portfolio-react/src/pages/Portfolio.tsx
@@ -6,24 +6,19 @@ import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import BackToTop from '../components/BackToTop';
-import PageTransition from '../components/PageTransition';
-import ScrollProgress from '../components/ScrollProgress';
 
 function Portfolio() {
   return (
-    <PageTransition transitionType="default">
-      <div className="min-h-screen">
-        <ScrollProgress />
-        <Navigation />
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-        <Footer />
-        <BackToTop />
-      </div>
-    </PageTransition>
+    <div className="min-h-screen">
+      <Navigation />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+      <BackToTop />
+    </div>
   );
 }
 
